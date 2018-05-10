24b74b34e97c90abc14d9384918564a7fa6d619e
mikel duke <mikelduke@gmail.com>
2018-05-09 20:49:50 -0500

    cleanup junk in app class

--- src/main/java/com/mikelduke/charlie/CharlieApplication.java
+++ src/main/java/com/mikelduke/charlie/CharlieApplication.java
@@ -2,12 +2,9 @@ package com.mikelduke.charlie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableAsync
-public class CharlieApplication implements WebMvcConfigurer {
+public class CharlieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CharlieApplication.class, args);
