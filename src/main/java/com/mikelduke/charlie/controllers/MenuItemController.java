3e67d3af9af9b04522820f3581656a232fe17bb6
mikel duke <mikelduke@gmail.com>
2018-05-15 21:43:12 -0500

    start a menu item edit page

--- src/main/java/com/mikelduke/charlie/controllers/MenuItemController.java
+++ src/main/java/com/mikelduke/charlie/controllers/MenuItemController.java
@@ -4,25 +4,47 @@ import com.mikelduke.charlie.model.MenuItem;
 import com.mikelduke.charlie.repositories.MenuItemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class MenuItemController {
 
     @Autowired
     MenuItemRepository menuItemRepository;
 
-    @GetMapping(value = "/menuitems")
-    public Iterable<MenuItem> getMenuItems() {
-        return menuItemRepository.findAll();
+    @GetMapping(value = "/menu")
+    public String getPages(Model model,
+            @RequestParam(name = "new", required = false, defaultValue = "false") boolean newMenuItem) {
+        if (newMenuItem) {
+            model.addAttribute("newMenuItem", true);
+            return "editMenuItem";
+        }
+
+        Iterable<MenuItem> menuItems = menuItemRepository.findAll();
+        model.addAttribute("menuItems", menuItems);
+
+        return "menuitems";
     }
 
-    @GetMapping(value = "/menuitems/{id}")
-    public MenuItem getMenuItem(@PathVariable("id") long id) {
-        return menuItemRepository
-                .findById(id)
+    @GetMapping(value = "/menu/{id}")
+    public String editPage(Model model,
+            @PathVariable Long id) {
+        MenuItem menuItem = menuItemRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("menu item not found"));
+        model.addAttribute("menuItem", menuItem);
+
+        return "editMenuItem";
+    }
+
+    @PostMapping(value = "/menu")
+    public String newPage(@ModelAttribute("menuitem") MenuItem menuItem) {
+        menuItemRepository.save(menuItem);
+        return "redirect:/menuitems";
     }
 }
